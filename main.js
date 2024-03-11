889d8b83c16f5124d48f0f62e3c19752180f957b
Revanth <147156884+Revanth-Krish@users.noreply.github.com>
2024-03-11 14:10:34 +0000

    first commit

---
+++ main.js
@@ -0,0 +1,13 @@
+const accesskey = " TbPbJU0n5GaN1d2_DL8lPIpGk2XNggztYT938d27tU0 "
+
+const formE1 = document.querySelector ("form")
+const inputE1 = document.getElementById("search-input")
+const secresult = document.querySelector(".image-results")
+const showmore = document.getElementById("load-buttons")
+
+let inputData =" "
+let page = 1;
+
+function imageAll(){
+    inputData
+}
