8be451ccc4584728550c15abf07410208165fee7
Revanth <147156884+Revanth-Krish@users.noreply.github.com>
2024-03-15 17:46:12 +0530

    Update main.js

--- main.js
+++ main.js
@@ -8,6 +8,54 @@ const showmore = document.getElementById("load-buttons")
 let inputData =" "
 let page = 1;
 
-function imageAll(){
-    inputData
+async function imageAll(){
+    inputData =inputE1.value;
+    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=${accesskey}&per_page=9`;
+    const response = await fetch(url);
+    const data = await response.json();
+
+
+    const results =data.results;
+
+if (page==1){
+    secresult.innerHTML = "";
 }
+
+results.map((result) =>{
+    const imageWrapper = document.createElement('div');
+    imageWrapper.classList.add("search-result");
+
+    const image = document.createElement('img');
+    image.src = result.urls.small;
+    image.alt = result.alt_description;
+
+    const imageLink = document.createElement('a');
+    imageLink.href = result.links.html;
+    imageLink.target ="_blank";
+    imageLink.textContent =result.alt_description;
+
+   
+    imageWrapper.appendChild(image);
+    imageWrapper.appendChild(imageLink);  
+    secresult.appendChild(imageWrapper);
+  });
+
+
+page++;
+if (page>1){
+    showmore.style.display ="block";
+   }
+} 
+
+formE1.addEventListener("submit", (event) =>{
+    event.preventDefault();
+    page=1;
+    imageAll();
+});
+
+
+
+showmore.addEventListener("click", () => {
+    imageAll();
+});
+
